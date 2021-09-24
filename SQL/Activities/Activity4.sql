d5f2b6f7cedd384b364e2fc121224001a97b05c9
AP181991 <91046080+AP181991@users.noreply.github.com>
2021-09-24 11:53:14 +0530

    Add files via upload

---
+++ SQL/Activities/Activity4.sql
@@ -0,0 +1,11 @@
+REM   Script: SQL_Activity4
+REM   SQL_Activity4
+
+
+
+ALTER TABLE salesman ADD grade int;
+
+UPDATE salesman SET grade=100;
+
+select * from salesman;
+
---
+++ SQL/Activities/Activity5.sql
@@ -0,0 +1,13 @@
+REM   Script: SQL_Activity5
+REM   SQL_Activity5
+
+
+
+UPDATE salesman set grade=200 where salesman_city='Rome';
+
+UPDATE salesman set grade=300 where salesman_name='Lauson Hen';
+
+UPDATE salesman SET salesman_name='Pierre' WHERE salesman_name='McLyon';
+
+SELECT * FROM salesman;
+
